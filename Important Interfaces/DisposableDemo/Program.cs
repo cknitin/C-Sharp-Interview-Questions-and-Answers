9875d6fd43b2c7480fbb5b1419cfb3a6cfc1212d
nkumar <nkumar@MCNDESKTOP36>
2018-03-06 14:38:11 +0530

    IDisposable Demo added

---
+++ Important Interfaces/DisposableDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableDemo
+{
+    class Demo : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Call dispose function.");
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = null;
+            try
+            {
+                d = new Demo();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                d.Dispose();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
