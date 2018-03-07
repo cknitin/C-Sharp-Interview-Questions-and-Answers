96379d079b8a9183a0ee929e6d31b87d98e779cb
nkumar <nkumar@MCNDESKTOP36>
2018-03-07 15:09:51 +0530

    Encapsulation & Data Hiding

--- ProtectedInternallDemoLibrary/Class1.cs
+++ ProtectedInternallDemoLibrary/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,10 @@ namespace ProtectedInternallDemoLibrary
         public void PrintGreeting()
         {
             Console.WriteLine(Greeting);
+
+
+
+
         }
     }
 }
---
+++ RefAndOutDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOutDemo
+{
+    class Demo
+    {
+        public void Print(ref string firstName, out string lastName)
+        {
+            firstName = "Steve";
+            lastName = "Smith";
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Tony";
+            string lastName = "Stark";
+
+            Demo d = new Demo();
+
+            Console.WriteLine($"Before -  {firstName} {lastName}");
+
+            d.Print(ref firstName,out lastName);
+
+            Console.WriteLine($"Afer -  {firstName} {lastName}");
+
+            Console.ReadLine();
+        }
+    }
+}
