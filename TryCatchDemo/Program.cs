b9addd8806d5fd18d1e20c1b55bb60c11feeaa09
nkumar <nkumar@MCNDESKTOP36>
2018-03-08 17:23:50 +0530

    Try & catch

---
+++ TryCatchDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    try
+                    {
+                        int x = 10;
+                        int y = 0;
+                        int z = x / y;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        Console.WriteLine("c Finally");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                }
+                finally
+                {
+                    Console.WriteLine("B Finally");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("A Finally");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
